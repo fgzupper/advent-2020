0d6e1389f30436e8fd30552ef59e82c641c5130b
Floyd Zupper <fgzupper@gmail.com>
2020-12-04 00:31:11 -0600

    Fix indentation and add comments
    This is putting lipstick on a pig.
    I should parse numbers first.

--- src/advent_2020/day_04.clj
+++ src/advent_2020/day_04.clj
@@ -29,7 +29,6 @@
         ist (set/intersection required-tokens fields)]
     (= (count ist) (count required-tokens))))
 
-
 (defn- height? [h]
   (or (->> (when (and (string? h)(re-matches #"\d{3}cm" h)) ["150cm" h "193cm"])
            sort
@@ -40,33 +39,41 @@
            ((juxt first last))
            (= ["59in" "76in"]))))
 
-;; sorry lol
+;; A collection of predicates to check.
+;; comp composes functions (right to left, or bottom to top)
+;; So basically, the first one pulls out the value for :byr,
+;; then does some nasty string comparisons to see if it is in a range.
+;; (never do that... parse your numbers)
 (def predicates
-  [
-(comp (partial = ["1920" "2002"])
-       (juxt first last)
-       #(when (= 4 (count %)) (sort ["1920" % "2002"])) :byr)
-(comp (partial = ["2010" "2020"])
-       (juxt first last)
-       #(sort ["2010" % "2020"]) :iyr)
-(comp (partial = ["2020" "2030"])
-       (juxt first last)
-       #(sort ["2020" % "2030"]) :eyr)
-(comp height? :hgt)
-(comp #(and (string? %) (re-matches #"#[0-9a-f]{6}" %)) :hcl)
+  [(comp (partial = ["1920" "2002"])
+         (juxt first last)
+         #(sort ["1920" % "2002"])
+         :byr)
+   (comp (partial = ["2010" "2020"])
+         (juxt first last)
+         #(sort ["2010" % "2020"])
+         :iyr)
+   (comp (partial = ["2020" "2030"])
+         (juxt first last)
+         #(sort ["2020" % "2030"])
+         :eyr)
+   (comp height? :hgt)
+   (comp #(and (string? %)
+               (re-matches #"#[0-9a-f]{6}" %)) :hcl)
    (comp #{"amb" "blu" "brn" "gry" "grn" "hzl" "oth"} :ecl)
    (comp #(and (string? %) (re-matches #"\d{9}" %)) :pid)
    ])
-((comp #{"a"} :a) {:a "a"})
-((comp #(and (string? %) (re-matches #"[0-9a-f]{6}" %)) :hcl) {})
-((comp #(and (string? %) (re-matches #"#[0-9a-f]{6}" %)) :hcl) {:hcl "#009aff"})
-
 
 (defn valid2? [pp]
   (let [tokens (str/split pp #"\s")
+        ;; Takes all the tokens (words in a passport),
+        ;; and throws them into a map of key-value pairs
+        ;; Example: {:byr "2001" :eyr "2021" ...}
         fields (->> tokens 
                     (keep #(-> % (str/split #":") ((fn [[f s]] [(keyword f) s]))) )
                     (into {}))]
+    ;; Once we have that map, we can run all of our predicates against it,
+    ;; and make sure every one returns something truthy.
     (->> 
          (map (fn [f m] (f m)) predicates (repeat fields))
          (every? identity))))
@@ -74,6 +81,6 @@
 (defn -main []
   (time
     (do 
-(println (count (filter valid? data)))
-(println (count (filter valid2? data)))
-)))
+      (println (count (filter valid? data)))
+      (println (count (filter valid2? data)))
+      )))
